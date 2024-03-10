366ae4bfc3b3987f252d43e4fe5f21109027abfe
androxxe <andriopratama16@gmail.com>
2024-03-10 20:41:58 +0700

    feat: wip sidebar menu

---
+++ src/app/general/page.tsx
@@ -0,0 +1,3 @@
+export default function Batak() {
+  return <div>Batak</div>;
+}
--- src/app/tech/layout.tsx
+++ src/app/tech/layout.tsx
@@ -1,10 +1,13 @@
-import { Footer } from "@/components/tech/organism";
+import { Footer, Sidebar } from "@/components/tech/organism";
 
 export default function TechLayout({ children }: { children: JSX.Element }) {
   return (
     <div className="bg-neutral-900 h-screen px-10 py-7">
       <div className="bg-neutral-950 h-full rounded-md flex flex-col">
-        <div className="p-5 flex flex-1 overflow-y-auto">{children}</div>
+        <div className="flex flex-1 overflow-y-auto">
+          <Sidebar />
+          <div className="p-5">{children}</div>
+        </div>
         <Footer />
       </div>
     </div>
