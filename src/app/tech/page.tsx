dbcdbed6ef84869deffe06d95280963cd33654d6
androxxe <andriopratama16@gmail.com>
2024-03-12 22:39:53 +0700

    feat: some rename on file and folder name

--- src/app/tech/page.tsx
+++ src/app/tech/page.tsx
@@ -1,14 +1,14 @@
 "use client";
 
-import { Section1, Section2 } from "@/components/tech/mdx";
+import { MdxSection1, MdxSection2 } from "@/components/tech/mdx";
 import { Gallery } from "@/components/tech/molecules";
 
 export default function Tech() {
   return (
     <div className="text-white">
-      <Section1 />
+      <MdxSection1 />
       <Gallery />
-      <Section2 />
+      <MdxSection2 />
       <div className="mt-12">
         <p className="text-sm text-slate-400">
           Built with <span className="text-red-500">♥︎</span> with Next.js, shadcn/ui, and next/mdx.
--- src/components/tech/mdx/index.ts
+++ src/components/tech/mdx/index.ts
@@ -1,3 +1,4 @@
-export { default as Section1 } from "./section-1.mdx";
-export { default as Section2 } from "./section-2.mdx";
+export { default as MdxSection1 } from "./section-1.mdx";
+export { default as MdxSection2 } from "./section-2.mdx";
 export { default as MdxGallery } from "./gallery.mdx";
+export { default as MdxCodeGalleryImages } from "./code/gallery-images.mdx";
