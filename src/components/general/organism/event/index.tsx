94a5ad965c31841aa562b1b8c0421c8d6dbf2b2f
androxxe <andriopratama16@gmail.com>
2024-03-23 23:26:25 +0700

    fix: add ulos on event card

--- src/components/general/organism/event/index.tsx
+++ src/components/general/organism/event/index.tsx
@@ -3,6 +3,7 @@ import { cn } from "@/lib/utils";
 import Countdown, { CountdownRenderProps } from "react-countdown";
 import dayjs from "dayjs";
 import Link from "next/link";
+import Image from "next/image";
 
 type ListEventProps = {
   title: string;
@@ -19,7 +20,7 @@ const ListEvent = (props: ListEventProps) => {
       <h2 className={cn(bonheurRoyale.className, "text-4xl text-maroon-700")}>{title}</h2>
       <p className={cn(ebGaramond.className, "text-lg")}>{date}</p>
       <p className={cn(ebGaramond.className, "text-lg")}>{time}</p>
-      <p className={cn(ebGaramond.className, "text-lg pt-2")}>{location}</p>
+      <p className={cn(ebGaramond.className, "text-lg")}>{location}</p>
     </div>
   );
 };
@@ -28,15 +29,15 @@ export const Event = () => {
   const renderer = ({ days, hours, minutes, seconds }: CountdownRenderProps) => {
     return (
       <div className="flex items-center space-x-2 justify-center mt-2">
-        <div className="w-20 h-20 bg-white rounded-md flex flex-col items-center justify-center">
+        <div className="w-20 h-20 bg-white border-maroon-400 rounded-md flex flex-col items-center justify-center">
           <span className={cn(ebGaramond.className, "text-xl font-medium")}>{days}</span>
           <span className="text-xs text-gray-500">Days</span>
         </div>
-        <div className="w-20 h-20 bg-white rounded-md flex flex-col items-center justify-center">
+        <div className="w-20 h-20 bg-white border-maroon-400 rounded-md flex flex-col items-center justify-center">
           <span className={cn(ebGaramond.className, "text-xl font-medium")}>{hours}</span>
           <span className="text-xs text-gray-500">Hours</span>
         </div>
-        <div className="w-20 h-20 bg-white rounded-md flex flex-col items-center justify-center">
+        <div className="w-20 h-20 bg-white border-maroon-400 rounded-md flex flex-col items-center justify-center">
           <span className={cn(ebGaramond.className, "text-xl font-medium")}>{minutes}</span>
           <span className="text-xs text-gray-500">Minutes</span>
         </div>
@@ -50,31 +51,51 @@ export const Event = () => {
 
   return (
     <div className="w-full h-full p-3 lg:p-12 bg-[url(/images/theme/general/main-pattern.jpg)] bg-[length:16rem_16rem]">
-      <div className="bg-gold-200 h-full rounded-md shadow-sm px-5 py-8 lg:py-12 overflow-y-auto">
-        <h2 className={cn(viaodaLibre.className, "text-4xl text-center mb-8")}>Save The Date</h2>
-        <ListEvent
-          title="Pemberkatan Nikah"
-          date="Sabtu, 20 April 2024"
-          time="09.30 - 11.30 WIB"
-          location="Gereja POUK TS1 PT. RAPP"
-        />
-        <ListEvent
-          title="Pesta Adat / Resepsi"
-          date="Sabtu, 20 April 2024"
-          time="12.00 WIB - Selesai"
-          location="Gedung Serbaguna Yeremia, Pkl. Kerinci"
-        />
-        <div>
-          <Countdown date={dayjs("2024-04-20T09:30:00+07:00").toDate()} renderer={renderer} />
-          <div className="flex items-center justify-center mt-5 lg:mt-7">
-            <Link
-              href={`https://www.google.com/calendar/render?action=TEMPLATE&text=Andrio%20&%20Rut%20Wedding&dates=20240420T093000/20240420T113000&details=Andrio%20&%20Rut%20Wedding`}
-              target="_blank"
-              className={cn(ebGaramond.className, "bg-maroon-400 px-3 py-1 rounded-lg text-center text-white")}
-            >
-              Add to Calendar
-            </Link>
+      <div className="bg-gold-200 h-full flex flex-col rounded-md shadow-sm">
+        <div className="relative w-full h-6">
+          <Image
+            src="/images/theme/general/home-top-ulos.jpg"
+            fill
+            alt="Ulos"
+            objectFit="cover"
+            className="rounded-t-md"
+          />
+        </div>
+        <div className="px-5 py-8 lg:py-10 flex-1 overflow-y-auto">
+          <h2 className={cn(viaodaLibre.className, "text-4xl text-center mb-4")}>Save The Date</h2>
+          <ListEvent
+            title="Pemberkatan Nikah"
+            date="Sabtu, 20 April 2024"
+            time="09.30 - 11.30 WIB"
+            location="Gereja POUK TS1 PT. RAPP"
+          />
+          <ListEvent
+            title="Pesta Adat / Resepsi"
+            date="Sabtu, 20 April 2024"
+            time="12.00 WIB - Selesai"
+            location="Gedung Serbaguna Yeremia, Pkl. Kerinci"
+          />
+          <div>
+            <Countdown date={dayjs("2024-04-20T09:30:00+07:00").toDate()} renderer={renderer} />
+            <div className="flex items-center justify-center mt-5 lg:mt-7">
+              <Link
+                href={`https://www.google.com/calendar/render?action=TEMPLATE&text=Andrio%20&%20Rut%20Wedding&dates=20240420T093000/20240420T113000&details=Andrio%20&%20Rut%20Wedding`}
+                target="_blank"
+                className={cn(ebGaramond.className, "bg-maroon-400 px-3 py-1 rounded-lg text-center text-white")}
+              >
+                Add to Calendar
+              </Link>
+            </div>
           </div>
+        </div>
+        <div className="relative w-full h-6">
+          <Image
+            src="/images/theme/general/home-top-ulos.jpg"
+            fill
+            alt="Ulos"
+            objectFit="cover"
+            className="rounded-b-md"
+          />
         </div>
       </div>
     </div>
