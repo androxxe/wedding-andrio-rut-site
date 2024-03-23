50657df2729c36a04224deebeee3d382b3252be3
androxxe <andriopratama16@gmail.com>
2024-03-23 21:38:55 +0700

    feat: wip groom screen

--- src/components/general/organism/groom/index.tsx
+++ src/components/general/organism/groom/index.tsx
@@ -5,8 +5,8 @@ import Link from "next/link";
 
 export const Groom = () => {
   return (
-    <div className="h-full w-full bg-[url(/images/theme/general/main-pattern.jpg)] bg-[length:16rem_16rem] flex flex-col overflow-y-auto">
-      <div className="text-right px-5 lg:pr-5 lg:pl-36 pt-12 lg:pt-20 space-y-2 z-10">
+    <div className="h-full w-full bg-[url(/images/theme/general/main-pattern.jpg)] bg-[length:16rem_16rem] flex flex-col items-end lg:justify-center overflow-y-auto">
+      <div className="text-right space-y-2 z-10 w-full lg:w-[24rem] px-5 mt-24 lg:mt-0">
         <h2 className={cn(viaodaLibre.className, "text-gold-200 text-3xl")}>Andrio Pratama Sirait</h2>
         <h4 className={cn(ebGaramond.className, "text-white text-lg")}>
           Anak ke 3 dari Alm. Drs. Hasiholan Sirait & Elfrida Br Sinurat
@@ -29,6 +29,36 @@ export const Groom = () => {
           fill
           objectFit="contain"
           alt="Groom Andrio Pratama Sirait"
+        />
+      </div>
+      <div className="w-32 lg:w-40 h-32 absolute top-[38rem] lg:top-[42rem] left-0 flex items-start justify-start z-10">
+        <Image
+          src="/images/theme/general/groom-flower-1.png"
+          fill
+          objectFit="contain"
+          objectPosition="left"
+          alt="Ornament"
+          className="bg-position-left text-left"
+        />
+      </div>
+      <div className="w-32 lg:w-40 h-32 absolute top-[38rem] lg:top-[42rem] left-12 lg:left-16 flex items-start justify-start">
+        <Image
+          src="/images/theme/general/groom-flower-2.png"
+          fill
+          objectFit="contain"
+          objectPosition="left"
+          alt="Ornament"
+          className="bg-position-left text-left"
+        />
+      </div>
+      <div className="w-32 h-48 absolute top-[14rem] left-0 flex items-start justify-start z-10">
+        <Image
+          src="/images/theme/general/groom-flower-3.png"
+          fill
+          objectFit="contain"
+          objectPosition="left"
+          alt="Ornament"
+          className="bg-position-left text-left"
         />
       </div>
     </div>
