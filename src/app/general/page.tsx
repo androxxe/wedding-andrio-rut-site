828619d4f1e84637b6933766a9ff51f3fe408ef7
androxxe <andriopratama16@gmail.com>
2024-03-23 14:29:37 +0700

    feat: wipgroom

--- src/app/general/page.tsx
+++ src/app/general/page.tsx
@@ -4,11 +4,11 @@ import { BottomTab, Groom, Cover, Home } from "@/components/general/organism";
 import { useState } from "react";
 
 export default function Batak() {
-  const [activeIndex, setActiveIndex] = useState<number>(0);
+  const [activeIndex, setActiveIndex] = useState<number>(1);
 
   return (
     <div className="flex flex-col flex-1 bg-red-500 h-full overflow-y-auto">
-      <div className="flex-1">
+      <div className="flex-1 overflow-y-auto">
         <Cover isOpen />
         {activeIndex === 0 && <Home />}
         {activeIndex === 1 && <Groom />}
--- src/components/general/organism/groom/index.tsx
+++ src/components/general/organism/groom/index.tsx
@@ -1,17 +1,34 @@
-import { ebGaramond } from "@/app/general/layout";
+import { bonheurRoyale, ebGaramond, viaodaLibre } from "@/app/general/layout";
 import { cn } from "@/lib/utils";
 import Image from "next/image";
+import Link from "next/link";
 
 export const Groom = () => {
   return (
-    <div className="h-full w-full bg-[url(/images/theme/general/main-pattern.jpg)] bg-[length:16rem_16rem] flex flex-col">
-      <div className="w-full h-14 relative">
+    <div className="h-full w-full bg-[url(/images/theme/general/main-pattern.jpg)] bg-[length:16rem_16rem] flex flex-col overflow-y-auto">
+      <div className="text-right px-5 lg:pr-5 lg:pl-36 pt-12 lg:pt-20 space-y-2 z-10">
+        <h2 className={cn(viaodaLibre.className, "text-gold-200 text-3xl")}>Andrio Pratama Sirait</h2>
+        <h4 className={cn(ebGaramond.className, "text-white text-lg")}>
+          Anak ke 3 dari Alm. Drs. Hasiholan Sirait & Elfrida Br Sinurat
+        </h4>
+        <div>
+          <div className="bg-white px-2 py-1 rounded-lg inline">
+            <Link
+              href="https://www.instagram.com/andrio.sirait/"
+              target="_blank"
+              className={cn(ebGaramond.className, "text-maroon-700 text-lg")}
+            >
+              @andrio.sirait
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div className="w-64 lg:w-80 h-[42rem] absolute top-24 -left-12">
         <Image
-          src="/images/theme/general/home-top-ulos.jpg"
-          alt="Pola Ulos"
+          src="/images/theme/general/groom-andrio.png"
           fill
-          objectFit="cover"
-          className="mt-[-20px]"
+          objectFit="contain"
+          alt="Groom Andrio Pratama Sirait"
         />
       </div>
     </div>
