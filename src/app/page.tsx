0ac92f358c6433c17476446c8aaae201f7af3a7a
androxxe <andriopratama16@gmail.com>
2024-03-28 01:54:24 +0700

    feat: base landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,41 +1,75 @@
 "use client";
 
+import { Button } from "@/components/tech/atoms/button";
 import { Label } from "@/components/tech/atoms/label";
 import {
   Select,
   SelectContent,
   SelectGroup,
   SelectItem,
-  SelectLabel,
   SelectTrigger,
   SelectValue
 } from "@/components/tech/atoms/select";
 import { LanguageType, useI18n } from "@/hooks/useI18n";
-import Link from "next/link";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { useCallback, useState } from "react";
 import { useTranslation } from "react-i18next";
+
+type MenuType = "general" | "tech";
 
 export default function Home() {
   const { t, i18n } = useTranslation(["general"]);
   const { changeLanguage } = useI18n();
+  const router = useRouter();
+
+  const [selected, setSelected] = useState<MenuType | undefined>(undefined);
+
+  const handleContinue = useCallback(() => {
+    if (!selected) return;
+    router.push(`/${selected}`);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [selected]);
 
   return (
-    <main className="flex items-center justify-center flex-col h-screen ">
-      <h4 className="text-xl font-normal mb-5">{t("general:whichSide")}</h4>
-      <div className="flex items-center flex-col lg:flex-row gap-4">
-        <Link
-          href="/general"
-          className="flex items-center justify-center border rounded border-gray-500 bg-white w-52 h-52"
-        >
-          <span className="text-base font-bold">{t("general:normalPerson")}</span>
-        </Link>
-        <Link
-          href="/tech"
-          className="flex items-center justify-center border rounded border-gray-500 bg-white w-52 h-52"
-        >
-          <span className="text-base font-bold">{t("general:itGeek")}</span>
-        </Link>
+    <main className="flex items-center justify-center flex-col h-screen bg-neutral-100 dark:bg-red-900">
+      <div className="px-5 text-center mb-10">
+        <h4 className="text-lg font-medium text-gray-700 mb-3">{t("general:whichSide")}</h4>
+        <p className="text-sm text-gray-600">{t("general:whichSideDescription")}</p>
       </div>
-      <div className="mt-12 w-52">
+      <div className="flex items-center flex-row space-x-4 mb-4 w-72">
+        <button
+          onClick={() => setSelected("general")}
+          className={cn(
+            "flex-1 flex items-center justify-center flex-col hover:bg-neutral-300 h-36 border-2 border-neutral-300 rounded-md",
+            selected === "general" ? "border-2 border-neutral-600" : ""
+          )}
+        >
+          <Image src="/images/people.png" width={42} height={42} className="object-contain mb-2" alt="Normal person" />
+          <span className={cn("text-sm text-neutral-700", selected === "general" ? "font-medium" : "")}>
+            {t("general:normalPerson")}
+          </span>
+        </button>
+        <button
+          onClick={() => setSelected("tech")}
+          className={cn(
+            "flex-1 flex items-center justify-center flex-col hover:bg-neutral-300 h-36 border-2 border-neutral-300 rounded-md",
+            selected === "tech" ? "border-2 border-neutral-600" : ""
+          )}
+        >
+          <Image src="/images/nerd.png" width={42} height={42} className="object-contain mb-2" alt="Nerd" />
+          <span className={cn("text-sm text-neutral-700", selected === "tech" ? "font-medium" : "")}>
+            {t("general:itGeek")}
+          </span>
+        </button>
+      </div>
+      <div className="w-72">
+        <Button onClick={handleContinue} className="w-full">
+          {t("general:continue")}
+        </Button>
+      </div>
+      <div className="mt-24 w-72">
         <Label>{t("general:chooseLanguage")}</Label>
         <Select onValueChange={(data) => changeLanguage(data as LanguageType)} value={i18n.language}>
           <SelectTrigger className="w-full">
